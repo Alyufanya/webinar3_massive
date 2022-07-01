6aae41a87e70b6a64e232978a1386d20bece6c0c
«Aliya <alyakalimova@mail.ru>
2022-07-01 19:19:22 +0600

    task002

--- task001/task002/Program.cs
+++ task001/task002/Program.cs
@@ -4,4 +4,19 @@
        //     string rev = string.Concat(num.Reverse());
        //     Console.WriteLine(num == rev ? "палиндром" : "не палиндром");
 
-//
+//Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("Введите координату по Х для первой точки: ");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату по Y для первой точки: ");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату по Х для второй точки: ");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату по Y для второй точки: ");
+double x2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату по Y для третьей точки: ");
+double y2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату по Y для третьей точки: ");
+double z2 = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt((x1 - x2) + (y1 - y2) + (z1 - z2));
+Console.WriteLine($"расстояние между точками {distance}");
